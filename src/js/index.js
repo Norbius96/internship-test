39434545c376ec6301a47446831afd7e7f17f884
Norbius96 <mrnorbius3@gmail.com>
2020-06-05 21:13:42 +0200

    simple validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,44 @@
 /* Here goes your JS code */
 
-const clickMe = document.querySelector('#show-popup-form');
+const clickMe = document.getElementById('show-popup-form');
+const popup = document.querySelector('#popup');
 
 
-clickMe.addEventListener('click', () => alert('ok'))
+
+clickMe.addEventListener('click', () => showPopup());
+
+const showPopup = () => {
+    clickMe.style.display = 'none';
+    popup.style.display = 'inline';
+}
+
+
+const close = document.querySelector('#close');
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+    clickMe.style.display = 'inline';
+});
+
+
+
+const form = document.querySelector('form');
+const popupContainer = document.querySelector('.popup-container');
+const mail = document.querySelector('#mail');
+const password = document.querySelector('#password');
+const checkbox = document.querySelector('#terms');
+const submitButton = document.querySelector('#submit');
+
+form.addEventListener('submit', (e) => validationForm(e));
+
+const validationForm = (e) => {
+    e.preventDefault();
+    if (mail.value === '' || password.value === '' || checkbox.checked === false) {
+        popupContainer.classList.add('shake');
+        setTimeout(() => {
+            popupContainer.classList.remove('shake');
+        }, 1000);
+    }
+}
+
+
